2b7e2a08042a3d5dcf3c65eee0f471c5b63f7541
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-02-21 19:16:56 +0300

    created onOff component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
 
 function hello() {
     //debugger
@@ -13,20 +14,24 @@ function hello() {
 function App(props: any) {
     return (
         <div>
-            <PageTitle title={"This is APP component"}/>
-            <PageTitle title={"My friends"}/>
-            Article1
-            <Rating value={3}/>
-            <Accordion title={"Menu"} collapsed={true}/>
-            <Accordion title={"Users"} collapsed={false}/>
-            Article2
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
-
+            {/*<PageTitle title={"This is APP component"}/>*/}
+            {/*<PageTitle title={"My friends"}/>*/}
+            {/*Article1*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Accordion title={"Menu"} collapsed={true}/>*/}
+            {/*<Accordion title={"Users"} collapsed={false}/>*/}
+            {/*Article2*/}
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+            <OnOff/>
+            <OnOff/>
+            <OnOff/>
+            {/*<OnOff on={true}/>*/}
+            {/*<OnOff on={false}/>*/}
         </div>
     );
 }
@@ -34,8 +39,9 @@ function App(props: any) {
 type PageTitlePropsType = {
     title: string
 }
+
 function PageTitle(props: PageTitlePropsType) {
-    return <h1>{ props.title }</h1>
+    return <h1>{props.title}</h1>
 }
 
 
---
+++ my-app/src/components/OnOff/OnOff.tsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+
+type PropsType = {
+    //on: boolean
+}
+
+const OnOff = (props: PropsType) => {
+    const [ on, setOn ] = useState(false);
+
+    const onStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px",
+        backgroundColor: on ? "green" : "white",
+    }
+    const offStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "2px",
+        padding: "2px",
+        backgroundColor: !on ? "red" : "white",
+    }
+    const indicatorStyle = {
+        width: "10px",
+        height: "10px",
+        borderRadius: "5px",
+        border: "1px solid black",
+        display: "inline-block",
+        backgroundColor: on ? "green" : "red",
+    }
+
+    return (
+        <div>
+            <div style={onStyle} onClick={ () => { setOn(true) }}>On</div>
+            <div style={offStyle} onClick={ () => { setOn(false) }}>Off</div>
+            <div style={indicatorStyle}></div>
+        </div>
+    );
+};
+
+export default OnOff;
