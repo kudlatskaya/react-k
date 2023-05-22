e0597519c99cef9c98261901a115bd5e79666a08
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-05-22 09:27:04 +0300

    useeffect, mount, update

--- my-app/src/components/useEffect.tsx
+++ my-app/src/components/useEffect.tsx
@@ -3,15 +3,35 @@ import s from './useState/useState.module.css';
 
 const UseEffect = () => {
     const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
 
     useEffect(() => {
+        console.log('component update');
+        document.title = counter.toString();
+    })
 
-    }, [])
+    useEffect(() => {
+        console.log('component mount');
+        document.title = counter.toString();
+    },[])
+
+    useEffect(() => {
+        console.log('component mount and update? when counter changed');
+        document.title = counter.toString();
+    }, [counter])
 
     return (
         <div>
             <div>{counter}</div>
-            <button className={s.button} onClick={() => {setCounter(counter + 1)}}>Counter</button>
+            <button className={s.button} onClick={() => {
+                setCounter(counter + 1)
+            }}>Counter
+            </button>
+            <div>{fake}</div>
+            <button className={s.button} onClick={() => {
+                setFake(fake + 1)
+            }}>Fake
+            </button>
         </div>
     );
 };
