6add0974d87ccb78c9b9292c899bb5c32e618908
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-05-22 11:31:26 +0300

    analog clock

--- my-app/src/components/useEffect/AnalogClock/Clock.tsx
+++ my-app/src/components/useEffect/AnalogClock/Clock.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
+import s from './Clock.module.css';
 
 type ClockPropsType = {
-    mode: string,
+    mode: 'digital' | 'analog',
 }
 
 const Clock: React.FC<ClockPropsType> = ({mode}) => {
@@ -18,9 +19,28 @@ const Clock: React.FC<ClockPropsType> = ({mode}) => {
 
     const time = date.toLocaleTimeString();
 
+    const secondsStyle = {
+        transform: `rotate(${date.getSeconds() * 6}deg)`
+    };
+    const minutesStyle = {
+        transform: `rotate(${date.getMinutes() * 6}deg)`
+    };
+    const hoursStyle = {
+        transform: `rotate(${date.getHours() * 30}deg)`
+    };
+
     return (
         <div>
-            <span>{time}</span>
+            {mode === 'digital' &&  <span>{time}</span>}
+
+            {mode === 'analog' &&
+                <div className={s.clock}>
+                    <div className={s.analogClock}>
+                        <div className={`${s.dial} ${s.seconds}`} style={secondsStyle} />
+                        <div className={`${s.dial} ${s.minutes}`} style={minutesStyle}/>
+                        <div className={`${s.dial} ${s.hours}`} style={hoursStyle}/>
+                </div>
+            </div>}
         </div>
     );
 };
